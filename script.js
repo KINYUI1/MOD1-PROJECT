5d254a289d1b6f9fd76b1d8e5fbf9617f5f2f6f1
clement <ndiclementkinyui@gmail.com>
2023-07-17 21:55:53 -0500

    commit

---
+++ script.js
@@ -0,0 +1,79 @@
+let guessedNumber = document.getElementById("GuessedNumber");
+let submit = document.querySelector("#submit");
+let responce = document.querySelector(".responce");
+let randomNumber = Math.floor(Math.random()*50);
+let count = document.querySelector('#count');
+let wins = document.querySelector('#wins');
+let loses = document.querySelector('#loses');
+let level = document.querySelector('#level');
+//background music
+// window.addEventListener('click',()=>{
+//     document.querySelector('#song').play();
+// })
+
+//temporal level variable
+let templevel = 1;
+// temporal wins variable
+let tempWins = 0;
+//temporal loses variable
+let temploses = 0;
+//temporal attemp count variable
+count.textContent = 5;
+
+
+
+console.log(randomNumber);
+
+submit.addEventListener("click",()=>{
+    count.textContent -= 1;
+    if(guessedNumber.value > 50 || guessedNumber.value < 1){
+        responce.textContent = "Number must be between 1 and 50.";
+    }
+    else if(guessedNumber.value < randomNumber){
+        responce.textContent = "Your Guess is less than actual.";
+    }else if(guessedNumber.value > randomNumber){
+        responce.textContent = "Your guess is greater than actual.";
+    }else if(guessedNumber.value == randomNumber){
+        responce.textContent = "CORRECT.";
+        tempWins = tempWins + 1;
+        wins.textContent = tempWins;
+        if(templevel === 1){
+            count.textContent = 5;
+        }else if(templevel === 2){
+            count.textContent = 4;
+        }else if(templevel === 3){
+            count.textContent = 3;
+        }
+        
+        randomNumber = Math.floor(Math.random()*50);
+        console.log(randomNumber);
+    }
+    if(tempWins === 3){
+        templevel +=1;
+        level.textContent = templevel;
+        tempWins = 0;
+        if(templevel === 2){
+            count.textContent = 4;
+            temploses = 0;
+        }else if(templevel === 3){
+            count.textContent = 3;
+            temploses = 0;
+
+        }
+
+        
+    }
+    if(count.textContent == 0){
+        temploses += 1;
+        randomNumber = Math.floor(Math.random()*50);
+        console.log(randomNumber);
+        loses.textContent = temploses;
+        count.textContent = 5;
+    }
+    if(temploses === 3){
+        window.alert(`SORRY YOU'VE LOST THE GAME`)
+        location.reload();
+    }
+})
+
+
